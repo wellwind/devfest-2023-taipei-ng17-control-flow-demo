2e94cf173736a175cece4867f823a52dcf723bae
Mike Huang <wellwind@mail2000.com.tw>
2023-12-12 21:57:08 +0800

    feat: simple layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule],
+  template: `
+    <mat-toolbar color="primary">
+      <span>Angular 17 Control Flow Demo</span>
+    </mat-toolbar>
+    <mat-sidenav-container class="h-[calc(100vh-64px)]">
+      <mat-sidenav [opened]="true" mode="side">
+        <div class="w-32 m-2">
+          <h3>Menu</h3>
+        </div>
+      </mat-sidenav>
+      <mat-sidenav-content>
+        <div class="m-2">
+          <router-outlet></router-outlet>
+        </div>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+  `,
 })
-export class AppComponent {
-  title = 'devfest-taipei-2023-angular17-control-flow';
-}
+export class AppComponent {}
